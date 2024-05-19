87475824fcfc9d0d868ccbeba75ff2c00f3092e4
Mohammad Abdullah <mhdabdullah204@gmail.com>
2024-05-20 00:13:42 +0300

    Conversion and Operations

---
+++ conop.js
@@ -0,0 +1,20 @@
+//Conversion And Operations
+
+let score = "3";
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumb = Number(score);
+console.log(typeof valueInNumb);
+
+let valueInString = String(score);
+console.log(typeof valueInString);
+
+// Nan +> not a number
+
+
+//Booleans
+// 1 is true and 0 is false
+// "" false
+// "hello" true
---
+++ datatypes.js
@@ -0,0 +1,20 @@
+"use strict" // treated as new version of js
+
+let name = "Abdullah" // String
+let height  = "5'5" // String
+let ageUpdate  = 22 // Number 
+let isLoggedIn  = false // Boolean (True or False)
+
+//Primitive DataTypes
+
+//string Double Quotes ("")
+//Boolean(True Or False)
+//bigint
+//null (standalon value) (empty)
+//undefined (declare varibale but not assigned value eg: let name; let age; )
+//symbol (Uniqueness)
+
+//Object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);  //object
